8e3d8ed9cb7563103bb30f98042366b15d2cc5a4
YutaroTobita <tarou.070314@gmail.com>
2019-09-12 17:58:23 +0900

    mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,21 +16,11 @@ class UsersController < ApplicationController
 
   def create
 
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
-    else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
-    end
-
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
