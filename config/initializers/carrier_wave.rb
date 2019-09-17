854530a3b260cd94c13eb92c2f55dcb953c74aa6
YutaroTobita <tarou.070314@gmail.com>
2019-09-17 22:01:53 +0900

    host2

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
       :provider              => 'AWS',
       :region                => ENV['S3_REGION'], # 例: 'ap-northeast-1'
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
       :aws_host_name => "s3-ap-northeast-1.amazonaws.com"
 
 
