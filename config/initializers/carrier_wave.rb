606e124cd2284f2cefb6644cdfbd898a1950d586
YutaroTobita <tarou.070314@gmail.com>
2019-09-17 21:08:42 +0900

    ike

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
 if Rails.env.production?
     CarrierWave.configure do |config|
       config.fog_credentials = {
-        # Amazon S3用の設定
-        :provider              => 'AWS',
-        :region                => ENV['ap-northeast-1'], # 例: 'ap-northeast-1'
-        :aws_access_key_id     => ENV['AKIAR36LMKGT6RKU33E7'],
-        :aws_secret_access_key => ENV['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']
-      }
-      config.fog_directory     =  ENV['railstuto12345']
+       # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'], # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
     end
   end
